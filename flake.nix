cf559e13a4f6a100a96a174242210fc657e0c391
0x0D <0xOD@proton.me>
2026-03-28 04:08:52 +0100

    build: migrate to flakes and go-task

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-parts,
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        perSystem =
+          {
+            pkgs,
+            lib,
+            self',
+            ...
+          }:
+          {
+            devShells.default = pkgs.mkShell {
+              packages = with pkgs; [
+                cmark
+                gomplate
+                htmlq
+                jq
+                go-task
+                minify
+                python3
+                (pkgs.writeShellScriptBin "parallel-moreutils" ''${pkgs.moreutils}/bin/parallel "$@"'')
+              ];
+
+              shellHook = ''
+                export MIME_TYPES_PATH="${pkgs.mailcap}/etc/mime.types"
+              '';
+            };
+          };
+      }
+    );
+}
